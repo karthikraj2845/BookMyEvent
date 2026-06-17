507dd29b23d3d2756a09f328d168e572055fa09e
PUDURI KARTHIK RAJ <karthikrajpuduri@gmail.com>
2026-06-17 16:18:24 +0530

    Route for home page

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("BookMyEvent API is running 🚀");
+});
 
 app.use('/api/auth', authRoutes); 
 // console.log(authRoutes);
