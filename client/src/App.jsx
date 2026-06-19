e1cc7a8598a036e36385042253b122743de06a3b
PUDURI KARTHIK RAJ <karthikrajpuduri@gmail.com>
2026-06-19 18:33:22 +0530

    case-sensitive issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Home from './pages/Home';
 import EventDetails from './pages/EventDetails';
 import Login from './pages/Login';
